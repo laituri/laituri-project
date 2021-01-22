b26849db6933efaffc6e91d2de9fb33f8070353b
teemukissa <tremppu@hotmail.com>
2021-01-22 13:22:16 +0200

    fix(df): checkbox enter issue fixed

--- libs/dynamic-form/src/lib/fields/checkbox/checkbox.module.ts
+++ libs/dynamic-form/src/lib/fields/checkbox/checkbox.module.ts
@@ -3,11 +3,17 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CheckboxComponent } from './checkbox.component';
 import { DynaCommonModule } from '../../common/common.module';
+import { MarkdownModule } from 'ngx-markdown';
 
 @NgModule({
   declarations: [CheckboxComponent],
   entryComponents: [CheckboxComponent],
   exports: [CheckboxComponent],
-  imports: [CommonModule, DynaCommonModule, ReactiveFormsModule],
+  imports: [
+    CommonModule,
+    DynaCommonModule,
+    ReactiveFormsModule,
+    MarkdownModule.forChild(),
+  ],
 })
 export class CheckboxModule {}
